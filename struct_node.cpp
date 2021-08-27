b4d1e2119195b424bfc434d78d42898acf6b6a27
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-08-27 20:06:03 +0600

    Update struct_node.cpp

--- struct_node.cpp
+++ struct_node.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+struct node{
+    string str;
+    int num;
+    double doub;
+    char s;
+
+};
+
+int main()
+{
+    // create a data type where you store
+    {string, int , double, char}
+
+    // wrong way of defining
+    node mrk;
+    mrk.str= "puock";
+    mrk.num = 25;
+    mrk.doub = 25.2;
+    mrk.s = 'M';
+
+
+
+
+    return 0;
+}
