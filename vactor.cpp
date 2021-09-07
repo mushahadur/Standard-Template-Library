3ba09b91c95cbe9697b0b3e195c2417603b5432c
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-09-07 11:22:58 +0600

    Update vactor.cpp

--- vactor.cpp
+++ vactor.cpp
@@ -1,3 +1,9 @@
+///! Game - Over.
+/** ========================================**
+ ** @Author:   Md. Mushahadur Rahaman Khan
+               ( City University of Bangladesh, CSE 49th )
+ ** @Category: The Standard Template Library. (vector)
+/** ========================================**/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
